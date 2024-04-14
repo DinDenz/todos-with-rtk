7e5792c04cd83ec4770e6d619663d8860d83c902
DinDenz <DinDenz@users.noreply.github.com>
2024-04-14 22:26:25 +0300

    more1

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,6 +1,11 @@
 import React from "react";
-import { removeTodo, toggleTodoComplete } from "../store/todoSlice";
+//import { removeTodo, toggleTodoComplete } from "../store/todoSlice";
 import { useDispatch } from "react-redux";
+import { deleteTodo, toggleStatus } from "../store/todoSlice";
+
+//ранее мы синхронно удаляли через removeTodo таску из браузера, но сейчас мы удаляем ее асинхронно на сервере
+//в таком случае мы используем асинхронный экшен deleteTodo, а в нем мы уже вызвали синхронный ремувТуду
+//чтобыудаление было и в браузере, см слайс
 
 const TodoItem = ({ id, completed, title }) => {
   const dispatch = useDispatch();
@@ -10,10 +15,10 @@ const TodoItem = ({ id, completed, title }) => {
       <input
         type="checkbox"
         checked={completed}
-        onChange={() => dispatch(toggleTodoComplete({id}))}
+        onChange={() => dispatch(toggleStatus(id))}
       />
       <span>{title}</span>
-      <span className="delete" onClick={() => dispatch(removeTodo({id}))}>
+      <span className="delete" onClick={() => dispatch(deleteTodo(id))}>
         X
       </span>
     </li>
--- src/store/todoSlice.js
+++ src/store/todoSlice.js
@@ -1,8 +1,10 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
+/*----------------------запрос за тудушками-----------------------------------------------------------------------------------------------*/
 export const fetchTodos = createAsyncThunk(
   "todos/fetchTodos",
-  async function (_, { rejectWithValue }) {//принимает первым параметром то что передаем через диспатч, но мы тут ничего не передаем
+  async function (_, { rejectWithValue }) {
+    //принимает первым параметром то что передаем через диспатч, но мы тут ничего не передаем
     try {
       const response = await fetch(
         "https://jsonplaceholder.typicode.com/todos?_limit=10"
@@ -13,11 +15,72 @@ export const fetchTodos = createAsyncThunk(
       const data = await response.json();
       return data;
     } catch (error) {
-      return rejectWithValue(error.message);//таким образом мы ловим ошибку и перекидываем ее в экстраредьюсер по обработке ошибки
+      return rejectWithValue(error.message); //таким образом мы ловим ошибку и перекидываем ее в экстраредьюсер по обработке ошибки
     }
   }
 ); //принимает название экшена и функцию
-
+/*-----------------------удаление тудушек через сервак------------------------------------------------------------------------------------------------*/
+//удаляем таску не только из памяти браузера а на сервере, поэтому делает фетч с удалением,
+//и если все окей - диспатчим removeTodo, чтобы удалить все с браузера как и ранее
+//rejectWithValue - для обработки ошибок, dispatch - обычный диспетчер
+export const deleteTodo = createAsyncThunk(
+  "todos/deleteTodo",
+  async function (id, { rejectWithValue, dispatch }) {
+    // тут мы уже принимает id
+    try {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/todos/${id}`,
+        {
+          method: "DELETE"
+        }
+      );
+      console.log(response);
+      if (!response.ok) {
+        throw new Error("Can't delete task. Server error");
+      }
+      dispatch(removeTodo({ id }));
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+/*--------------хэлпер на ошибки(одинаковый для обоих функицй)------------------------------------------------------*/
+const setError = (state, action) => {
+  state.status = "rejected";
+  state.error = action.payload;
+};
+/*-----------------изменение статуса тудушки--------------------------------------------------------------------------------------------------*/
+//нам надо обновить поле completed, чтобы его обновить,надо получить значение того, что есть сейчас
+//для этого есть getState, который возвращает общий стэйт, через него получаем актуальный completed
+export const toggleStatus = createAsyncThunk(
+  "todos/toggleStatus",
+  async function (id, { rejectWithValue, dispatch, getState }) {
+    const todo = getState().todos.todos.find((todo) => todo.id === id);
+    try {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/todos/${id}`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            completed: !todo.completed
+          })
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Can't toggle task status. Server error");
+      }
+      /* const data = await response.json();
+      console.log(data); */ // чекаем ответ сервера
+      dispatch(toggleTodoComplete({ id })); //изменяем UI в соответсвии с изменениями на сервере
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+/*-------------------------------------------------------------------------------------------------------------------------*/
 const todoSlice = createSlice({
   name: "todos",
   initialState: {
@@ -54,10 +117,10 @@ const todoSlice = createSlice({
       state.status = "resolved";
       state.todos = action.payload;
     });
-    builder.addCase(fetchTodos.rejected, (state, action) => {
-      state.status = 'rejected';
-      state.error = action.payload;
-    });
+    builder.addCase(fetchTodos.rejected, setError);
+    builder.addCase(deleteTodo.rejected, setError);
+    builder.addCase(toggleStatus.rejected, setError);
+    //можно сделать так чтобы сообщение об ошибке стиралось с экрана в случае пендинга
   }
 });
 
